2c9a48cf7c075f878b930921f01272c04f08ad5e
Darek Frynas <darekfrynas@gmail.com>
2018-09-25 09:35:42 +0200

    Main app component with store initialized

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,9 +1,20 @@
 import React, { Component } from 'react';
+import { observer } from 'mobx-react';
+
+import TodoList from '../Todo/List'
+import TodoListStore from '../Todo/List/store'
 
 
+const store = new TodoListStore();
+
+@observer
 class App extends Component {
     render() {
-        return <div/>;
+        return (
+            <div className="container">
+                <TodoList store={store} />
+            </div>
+        );
     }
 }
 
