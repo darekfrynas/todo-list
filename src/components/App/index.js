d4e1a57b82aa3c445e346acada8f69a4cab48538
Darek Frynas <darekfrynas@gmail.com>
2018-09-26 12:09:26 +0200

    Styles organization refactor & background
    - keep global styles in assets dir
    - load global styles in index.js
    - define app background

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -5,15 +5,22 @@ import TopBar from '../Todo/TopBar'
 import TodoList from '../Todo/List'
 import AddNewItemForm from '../Todo/AddNew'
 
+import './styles.scss';
+
 
 class App extends Component {
     render() {
         return (
-            <div className="container">
-                <TopBar />
-                <TodoList />
-                <AddNewItemForm />
-                <DevTools />
+            <div class="container">
+                <div styleName="app-container">
+                    <h1>Boost your productivity with this simple tool</h1>
+                    <hr className="mb-5" />
+
+                    <TopBar />
+                    <TodoList />
+                    <AddNewItemForm />
+                    <DevTools />
+                </div>
             </div>
         );
     }
--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import { Provider } from 'mobx-react';
 import App from './components/App';
 import TodoListStore from './components/Todo/List/store'
 import registerServiceWorker from './registerServiceWorker';
+import './assets/styles/bootstrap.global.scss';
+import './assets/styles/app.global.scss';
 
 
 const store = new TodoListStore();
